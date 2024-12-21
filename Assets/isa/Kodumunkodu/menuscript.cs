4aabbf81ddd6bbc947fef2269890a21167dd93a8
Glachus <69464142+Glachus@users.noreply.github.com>
2024-12-21 18:29:06 +0300

    menu scene, pause scene

--- Assets/isa/Kodumunkodu/menuscript.cs
+++ Assets/isa/Kodumunkodu/menuscript.cs
@@ -3,12 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 
 public class menuscript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+    public void test()
+    {
+        Debug.Log("Pressed");
+    }
     public void StartGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
     }
 
     public void ExitGame()
@@ -19,5 +60,11 @@ public class menuscript : MonoBehaviour
     public void SetVolume(float volume)
     {
         Debug.Log(volume);
+        audioMixer.SetFloat("mastervolume", volume);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+            Screen.fullScreen = isFullscreen;
     }
 }
